9c635e0257220ffa2eef94530a71a4e2ac3c4314
Milan Patel <milan@pardypanda.com>
2020-12-28 00:31:03 +0530

    add dashboard test route

--- src/app/HomeView/index.js
+++ src/app/HomeView/index.js
@@ -183,7 +183,7 @@ const HomeView = () => {
                   component={NavLink}
                   color="primary"
                   variant="contained"
-                  to={"dashboard"}
+                  to={"/dashboard"}
                 >
                   <Typography display="inline" variant="subtitle2">
                     <Box px={4} py={1}>
