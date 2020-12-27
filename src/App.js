5597d8424fa86300042d88caa29292ca6f08194c
Milan Patel <milan@pardypanda.com>
2020-12-28 01:57:03 +0530

    revert back history version to 4.10.1 from 5.0.0 for navigation bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import { history } from "utils/history";
 import HomeView from "./app/HomeView";
 import DashboardView from "./app/DashboardView";
-import { Route, Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { theme } from "./Theme";
 
 const App = () => {
@@ -10,12 +10,10 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router history={history}>
-        <Route path="/" exact render={(props) => <HomeView {...props} />} />
-        <Route
-          path="/dashboard"
-          exact
-          render={(props) => <DashboardView {...props} />}
-        />
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route path="/dashboard" component={DashboardView} />
+        </Switch>
       </Router>
     </ThemeProvider>
   );
