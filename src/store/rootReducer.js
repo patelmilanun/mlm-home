34c83191b164617fcdeffe7c91212c2d0f7dfe04
Milan Patel <milan@pardypanda.com>
2021-03-16 22:50:01 +0530

    add redux store, add common slice which contain state related to app layout like title, sidebar type, is full screen

---
+++ src/store/rootReducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "@reduxjs/toolkit";
+// import { reducer as authReducer } from 'slices/auth';
+import { reducer as commonReducer } from "slices/common";
+// import { reducer as sessionReducer } from 'slices/session';
+// import { reducer as userReducer } from 'slices/user';
+// import { reducer as paymentReducer } from 'slices/payment';
+
+const rootReducer = combineReducers({
+  // auth: authReducer,
+  common: commonReducer,
+  // session: sessionReducer,
+  // user: userReducer,
+  // payment: paymentReducer
+});
+
+export default rootReducer;
