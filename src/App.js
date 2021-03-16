5520fd566f1b28bc5cbe1d361b8eaa67ba530b96
Milan Patel <milan@pardypanda.com>
2021-03-16 22:42:06 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import { history } from "utils/history";
 import routes, { renderRoutes } from "routes/routes";
-// import HomeView from "./app/HomeView";
-// import DashboardView from "./app/DashboardView";
-import { Route, Router, Switch } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { theme } from "./Theme";
-import { theme2 } from "./Theme2";
 
 const App = () => {
   return (
