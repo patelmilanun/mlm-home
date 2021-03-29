232be24d9f256550c7eb4a9482a532df11d5a6c9
Milan Patel <milan@pardypanda.com>
2021-03-30 01:00:44 +0530

    add validation in form, add submit event to form, add multiple steps for signup

--- src/views/auth/RegisterView/index.js
+++ src/views/auth/RegisterView/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {
   Typography,
@@ -16,18 +17,15 @@ import {
   Hidden,
   useMediaQuery,
   TextField,
+  CircularProgress,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import {
-  Share2,
-  DollarSign,
-  Users,
-  Check,
-  ExternalLink,
-  ArrowRight,
-  PlayCircle,
-  LogIn,
-} from "react-feather";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "store";
+import { signup } from "slices/auth";
+import { Lock, LogIn } from "react-feather";
 
 const useStyles = makeStyles((theme) => ({
   logo: {
@@ -41,11 +39,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Full name is required"),
+  phoneNumber: yup
+    .string()
+    .required("Email is required")
+    .matches(/^[6789]\d{9}$/, "Phone number is invalid"),
+  email: yup.string().required("Email is required").email("Email is invalid"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      "Password must contains atleast 1 latter and 1 number"
+    ),
+});
+
 const RegisterView = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("sm"), {
     defaultMatches: true,
+  });
+
+  const [step, setStep] = useState(2);
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(validationSchema),
   });
 
   return (
@@ -56,7 +78,13 @@ const RegisterView = () => {
             <Card>
               <CardContent style={{ padding: 0 }}>
                 <Grid container justify="center">
-                  <Grid item xs={12} sm={8} md={6}>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={6}
+                    style={{ minHeight: "700px" }}
+                  >
                     <Box py={4} px={isSmall ? 4 : 6} textAlign="center">
                       <Box pb={4}>
                         <img
@@ -69,70 +97,203 @@ const RegisterView = () => {
                         Sign Up For Inspireonic
                       </Typography>
                       <br />
-                      <Box my={2}>
-                        <TextField
-                          id="referal"
-                          name="referal"
-                          label="Referal Id"
-                          variant="outlined"
-                          fullWidth
-                        />
-                      </Box>
-                      <Box my={2}>
-                        <TextField
-                          id="fname"
-                          name="fname"
-                          label="Full Name"
-                          variant="outlined"
-                          fullWidth
-                        />
-                      </Box>
-                      <Box my={2}>
-                        <TextField
-                          id="email"
-                          name="email"
-                          label="Email"
-                          variant="outlined"
-                          fullWidth
-                        />
-                      </Box>
-                      <Box my={2}>
-                        <TextField
-                          id="password"
-                          name="password"
-                          label="Password"
-                          type="password"
-                          variant="outlined"
-                          fullWidth
-                        />
-                      </Box>
-                      <Button
-                        color="primary"
-                        variant="contained"
-                        fullWidth
-                        startIcon={<LogIn />}
-                      >
-                        <Box my={1}>
-                          <Typography variant="subtitle2">Sign Up</Typography>
-                        </Box>
-                      </Button>
-                      <Box my={2}>
-                        <Typography variant="caption">
-                          <Link component={NavLink} to="/" underline="always">
-                            Forgot Password
-                          </Link>
-                        </Typography>
-                      </Box>
-                      <Typography variant="body1">
-                        Already have an account?{" "}
-                        <Link
-                          component={NavLink}
-                          to="/auth/login"
-                          underline="always"
-                        >
-                          Log In
-                        </Link>
-                      </Typography>
+                      {step === 0 && (
+                        <>
+                          <form
+                            noValidate
+                            onSubmit={handleSubmit(async (data) => {
+                              await dispatch(signup(data));
+                              setStep(step + 1);
+                            })}
+                          >
+                            <Box my={2}>
+                              <TextField
+                                id="name"
+                                name="name"
+                                label="Full Name"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                inputRef={register}
+                                error={!!errors.name}
+                                helperText={errors.name?.message}
+                              />
+                            </Box>
+                            <Box my={2}>
+                              <TextField
+                                id="email"
+                                name="email"
+                                label="Email"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                inputRef={register}
+                                error={!!errors.email}
+                                helperText={errors.email?.message}
+                              />
+                            </Box>
+                            <Box my={2}>
+                              <TextField
+                                id="password"
+                                name="password"
+                                label="Password"
+                                type="password"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                inputRef={register}
+                                error={!!errors.password}
+                                helperText={errors.password?.message}
+                              />
+                            </Box>
+                            <Box my={2}>
+                              <TextField
+                                id="phoneNumber"
+                                name="phoneNumber"
+                                label="Contact Number"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                inputRef={register}
+                                error={!!errors.phoneNumber}
+                                helperText={
+                                  errors.phoneNumber?.message ||
+                                  "without country code"
+                                }
+                              />
+                            </Box>
+                            <Box my={2}>
+                              <TextField
+                                id="referedBy"
+                                name="referedBy"
+                                label="Referal Id"
+                                variant="outlined"
+                                fullWidth
+                                inputRef={register}
+                                error={!!errors.referedBy}
+                                helperText={
+                                  errors.referedBy?.message ||
+                                  "Leave referal id blank if you dont have"
+                                }
+                              />
+                            </Box>
+                            <Button
+                              color="primary"
+                              variant="contained"
+                              type="submit"
+                              fullWidth
+                              startIcon={<LogIn />}
+                              onClick={() => setStep(1)}
+                            >
+                              <Box my={1}>
+                                <Typography variant="subtitle2">
+                                  Sign Up
+                                </Typography>
+                              </Box>
+                            </Button>
+                          </form>
+                          <Box my={2}>
+                            <Typography variant="caption">
+                              <Link
+                                component={NavLink}
+                                to="/"
+                                underline="always"
+                              >
+                                Forgot Password
+                              </Link>
+                            </Typography>
+                          </Box>
+                          <Typography variant="body1">
+                            Already have an account?{" "}
+                            <Link
+                              component={NavLink}
+                              to="/auth/login"
+                              underline="always"
+                            >
+                              Log In
+                            </Link>
+                          </Typography>
+                        </>
+                      )}
+                      {step === 1 && (
+                        <>
+                          <Box my={8}>
+                            <Typography variant="subtitle2">
+                              Please complete your payment to activate your
+                              account
+                            </Typography>
+                            <br />
+                            <br />
+                            <Typography variant="h1">₹ 100</Typography>
+                            <br />
+                            <Typography variant="subtitle1">
+                              Registration fee
+                            </Typography>
+                          </Box>
+                          <Box mb={2}>
+                            <Button
+                              color="primary"
+                              variant="contained"
+                              type="submit"
+                              fullWidth
+                              startIcon={<Lock />}
+                            >
+                              <Box my={1}>
+                                <Typography variant="subtitle2">
+                                  Pay Now
+                                </Typography>
+                              </Box>
+                            </Button>
+                          </Box>
+                          <Typography variant="caption">
+                            *all payment made through are secure
+                          </Typography>
+                        </>
+                      )}
+                      {step === 2 && (
+                        <>
+                          <Box my={8}>
+                            <Typography variant="subtitle2">
+                              Once you complete payment, we'll show you the
+                              status here. Next status check in
+                            </Typography>
+                            <br />
+                            <br />
+                            <Typography variant="h1">59 sec</Typography>
+                            <br />
+                            <Link
+                              component="button"
+                              variant="subtitle1"
+                              onClick={() => {
+                                console.info("I'm a button.");
+                              }}
+                            >
+                              check now
+                            </Link>
+                          </Box>
+                          <Box mb={2}>
+                            <Button
+                              color="primary"
+                              variant="contained"
+                              fullWidth
+                              disabled
+                              startIcon={
+                                <CircularProgress size={20} color="primary" />
+                              }
+                            >
+                              <Box my={1} mx={1}>
+                                <Typography variant="subtitle2">
+                                  Please Wait
+                                </Typography>
+                              </Box>
+                            </Button>
+                          </Box>
+                          <Typography variant="caption">
+                            *we'll update the button with appropriate payment
+                            status
+                          </Typography>
+                        </>
+                      )}
                     </Box>
                   </Grid>
                   <Hidden smDown>
